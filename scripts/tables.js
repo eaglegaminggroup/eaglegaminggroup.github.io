a3f5ee2c0f96bddc3d3fcd75de119cca7b05628b
Tanya <tanya.murrow@hobbylobby.com>
2016-09-22 14:35:07 -0500

    Clean and document tables script

--- scripts/tables.js
+++ scripts/tables.js
@@ -1,47 +1,29 @@
 $(window).load( function(){
+  // get today
   var today = new Date();
   var day = today.getDay();
   var hour = today.getHours();
+  
+  // if between Thursday at 5pm and Friday at midnight
   if(day === 4 && hour >= 17 || day === 5)
   {
-    //checkAuth();
     gapi.load('client', init);
   }
   else {
+    // show unavailable element
     $('.tables-unavailable').removeClass('hidden');
   }
 });
 
-var CLIENT_ID = '1062258359464-ue4mc5hk6fqrbamjb25m8h2svn406pv8.apps.googleusercontent.com';
-var SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
-
-// function checkAuth() {
-//   gapi.auth.authorize(
-//     {
-//       'client_id': CLIENT_ID,
-//       'scope': SCOPES.join(' '),
-//       'immediate': true
-//     }, handleAuthResult);
-// }
-// 
-// function handleAuthResult(authResult) {
-//   if (authResult && !authResult.error) {
-//     loadSheetsApi();
-//   }
-// }
-// 
-// function loadSheetsApi() {
-//   var discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
-//   gapi.client.load(discoveryUrl).then(readTables);
-// }
-
+// set api key and load 
 function init() {
   gapi.client.setApiKey('AIzaSyClqptXueR8OxUDbzJj6l0DRvKLf0eQle4');
   var discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
   gapi.client.load(discoveryUrl).then(readTables);
 }
 
- var tables = [];
+// array of tables
+var tables = [];
 
 function readTables() {
   gapi.client.sheets.spreadsheets.values.batchGet({
@@ -55,55 +37,84 @@ function readTables() {
     ]
   }).then(function(response) {
     var range = response.result;
+    // list of tables
     var tablesList = range.valueRanges;
     if (tablesList.length > 0) {
-      for(i = 0; i< tablesList.length; i++)
+      for(i = 0; i < tablesList.length; i++)
       {
-        var table = [];
-        if(tablesList[i].values.length > 0)
+        // list of players = a single table
+        var playersList = tablesList[i].values;
+        // table object
+        var table = {};
+        if(playersList.length > 2)
         {
-          for(j = 0; j < tablesList[i].values.length; j++)
+          // add dm to table object
+          table.dm = playersList[0][0].substring(8);
+          table.players = [];
+          for(j = 1; j < playersList.length; j++)
           {
-            var row = tablesList[i].values[j];
+            // single player = array of name and class
+            var row = playersList[j];
             if(row[0] !== undefined && row[0] !== 'Player')
             {
+              // add player to player list
               var player = {name: row[0], role: row[1]};
-              table.push(player);
+              table.players.push(player);
             }
           }
           tables.push(table);
         }
       }
     } else {
-      //appendPre('No data found.');
+      logError('No data found.');
     }
   }, function(response) {
-    //appendPre('Error: ' + response.result.error.message);
+    logError('Error: ' + response.result.error.message);
   }).then(function() {
     showTables();
+    // var out = '';
+    // for(i = 0; i < tables.length; i++)
+    // {
+    //   out += '\nTable ' + (i+1) + ': ' + tables[i].dm;
+    //   for(j = 0; j < tables[i].players.length; j++)
+    //   {
+    //     out += '\n' + tables[i].players[j].name + ' - ' + tables[i].players[j].role;
+    //   }
+    // }
+    // alert(out);
   });
 }
 
 function showTables() {
-  var output = '';
-  if(tables.length > 0)
+  // create the row
+  var output = '<div class="row">';
+  for(i = 0; i < tables.length; i++)
   {
-    output += '<div class="row">';
-    for(i = 0; i < tables.length; i++)
+    if(tables[i].players.length > 0)
     {
-      if(tables[i].length > 1)
+      // create a div col and list group and add the DM name and num players as first primary item
+      output += '<div class="col-md-4 col-sm-6"><ul class="list-group">';
+      output += '<li class="list-group-item list-group-item-primary"><span class="badge badge-primary">' + tables[i].players.length + '</span> ' + tables[i].dm + '</li>';
+      for(j = 0; j < tables[i].players.length; j++)
       {
-        output += '<div class="col-md-4 col-sm-6"><ul class="list-group">';
-        output += '<li class="list-group-item list-group-item-primary"><span class="badge badge-primary">' + (tables[i].length-1) + '</span> ' + tables[i][0].name.substring(8) + '</li>';
-        for(j = 1; j < tables[i].length; j++)
-        {
-          var player = tables[i][j];
-          output += '<li class="list-group-item"><span class="label label-default label-' + player.role.toLowerCase() + ' pull-right">' + player.role + '</span> ' + player.name + '</li>';
-        }
-        output += '</ul></div>';
-      }      
-    }
-    output += '</div>';
+        // add each player and their class to the list group
+        var player = tables[i].players[j];
+        output += '<li class="list-group-item"><span class="label label-default label-' + player.role.toLowerCase() + ' pull-right">' + player.role + '</span> ' + player.name + '</li>';
+      }
+      // close list group and div col
+      output += '</ul></div>';
+    }      
   }
+  // close row
+  output += '</div>';
+  // append elements to the tables div
   $('#tables').append(output);
+}
+
+function logError(message)
+{
+  if(window.console && window.console.log)
+  {
+    console.log(message);
+  }
 }
